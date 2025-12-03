fdc5877fa636263c02722dac907fff542fb313a7
jamesd <jamie.jj.jd@gmail.com>
2025-12-03 17:51:02 +0000

    change the sentence for testing purposes

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ export default {
 
     // 2. Define the Text to Analyze
     // This is the input that simulates a user comment or message.
-    const input_text = "I love how fast this Cloudflare Worker is!";
+    const input_text = "The weather makes me sad today.";
 
     // Define the inputs object for the model
     const inputs = {
